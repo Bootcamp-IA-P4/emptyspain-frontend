067fca65dd6b4b44792e1d12ff2dc0201f68989e
Omar Lengua <omarns21@gmail.com>
2025-03-04 13:14:46 +0100

    Implement: add components, and homepage

---
+++ src/app/components/FormTask.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FormTask = () => {
+  return (
+    <div>
+        <h2>Form Task</h2>
+    </div>
+  )
+}
+
+export default FormTask
---
+++ src/app/page.jsx
@@ -0,0 +1,18 @@
+import FormTask from "./components/FormTask"
+import ListTask from "./components/ListTask"
+
+function Homepage() {
+  return (
+    <div>
+        <h1>Tasks App</h1>
+
+        <FormTask />
+
+        <ListTask />
+  
+      
+    </div>
+  )
+}
+
+export default Homepage;
