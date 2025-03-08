3cd00a7d0965988a9759b777f9b181732c85104f
Andrea Alonso <andreacor001@gmail.com>
2025-03-08 16:26:38 +0100

    fix: toggle like on favorites page

--- src/app/favorites/page.js
+++ src/app/favorites/page.js
@@ -2,13 +2,18 @@
 import { useEffect, useState } from "react";
 import { useAuth } from "../../context/authContext";
 import { useRouter } from "next/navigation";
-import { getFavoritos, getPuebloById } from "../../services/api";
+import {
+  getFavoritos,
+  getPuebloById,
+  toggleFavorito,
+} from "../../services/api";
 import TownCard from "../components/TownCard";
 
 export default function Favorites() {
   const { user, loading } = useAuth();
   const router = useRouter();
   const [favoritos, setFavoritos] = useState([]);
+  const [isLoadingFavorites, setIsLoadingFavorites] = useState(true);
 
   useEffect(() => {
     if (!loading && !user) {
@@ -19,17 +24,35 @@ export default function Favorites() {
   useEffect(() => {
     const fetchFavorites = async () => {
       if (user) {
+        setIsLoadingFavorites(true);
         const favoritosData = await getFavoritos(user.id);
         const pueblosData = await Promise.all(
           favoritosData.map(async (fav) => await getPuebloById(fav.pueblo))
         );
         setFavoritos(pueblosData);
+        setIsLoadingFavorites(false);
       }
     };
     fetchFavorites();
   }, [user]);
 
-  if (loading) return <p>Cargando...</p>;
+  const handleFavoriteToggle = async (puebloId) => {
+    if (!user || user.tipo_usuario !== "visualizador") return;
+
+    const updatedFavoritos = favoritos.filter(
+      (pueblo) => pueblo.id !== puebloId
+    );
+    setFavoritos(updatedFavoritos);
+
+    await toggleFavorito(user.id, puebloId);
+  };
+
+  if (loading || isLoadingFavorites)
+    return (
+      <p className="text-center text-lg text-gray-600 mt-20 animate-pulse">
+        Cargando...
+      </p>
+    );
 
   return (
     <div className="min-h-screen bg-gray-100 p-8">
@@ -43,11 +66,12 @@ export default function Favorites() {
               key={pueblo.id}
               pueblo={pueblo}
               isFavorite={true}
+              onFavoriteToggle={handleFavoriteToggle}
               user={user}
             />
           ))
         ) : (
-          <p className="text-center text-gray-500">
+          <p className="text-center text-gray-500 mt-10">
             No tienes pueblos favoritos aún.
           </p>
         )}
